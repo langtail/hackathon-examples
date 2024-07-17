38bb521f8943d71fcc2f397d4bd358bc520703d2
Orest Khanenya <o.khanenya@gmail.com>
2024-07-17 12:52:25 +0200

    - connect youtube-dl-exec" - add get info muattion

--- examples/langtail-youtube-context/pages/index.tsx
+++ examples/langtail-youtube-context/pages/index.tsx
@@ -1,13 +1,37 @@
+import { FormEvent } from "react";
 import { trpc } from "../utils/trpc";
 
 export default function IndexPage() {
-  const hello = trpc.hello.useQuery({ text: "orest" });
-  if (!hello.data) {
-    return <div>Loading...</div>;
-  }
+  const getVideoInfo = trpc.getVideoInfo.useMutation();
+
+  const handleProcessVideo = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (e.target) {
+      const formData = new FormData(e.target as HTMLFormElement);
+      const url = formData.get("url") as string;
+
+      if (url) {
+        getVideoInfo.mutate({
+          url,
+        });
+      }
+    }
+  };
+
   return (
-    <div>
-      <p>{hello.data.greeting}</p>
-    </div>
+    <form onSubmit={handleProcessVideo}>
+      <input
+        name="url"
+        type="text"
+        placeholder="Enter youtube url"
+        disabled={getVideoInfo.isPending}
+      />
+
+      <button type="submit" disabled={getVideoInfo.isPending}>
+        {getVideoInfo.isPending ? "Processing" : "Process"}
+      </button>
+
+      {getVideoInfo.data && <p>{JSON.stringify(getVideoInfo.data, null, 2)}</p>}
+    </form>
   );
 }
--- examples/langtail-youtube-context/server/routers/_app.ts
+++ examples/langtail-youtube-context/server/routers/_app.ts
@@ -1,17 +1,27 @@
 import { z } from "zod";
 import { procedure, router } from "../trpc";
+import youtubeDl from "youtube-dl-exec";
 
 export const appRouter = router({
-  hello: procedure
+  getVideoInfo: procedure
     .input(
       z.object({
-        text: z.string(),
+        url: z.string(),
       })
     )
-    .query((opts) => {
-      return {
-        greeting: `hello ${opts.input.text}`,
-      };
+    .mutation(async (opts) => {
+      const info = await youtubeDl(
+        "https://www.youtube.com/watch?v=6xKWiCMKKJg",
+        {
+          dumpSingleJson: true,
+          noCheckCertificates: true,
+          noWarnings: true,
+          preferFreeFormats: true,
+          addHeader: ["referer:youtube.com", "user-agent:googlebot"],
+        }
+      );
+
+      return info;
     }),
 });
 
